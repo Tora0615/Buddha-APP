4972d4961cb95453efce5d1bd1845e59378ae127
Tora0615 <5438arthur@gmail.com>
2021-07-07 17:37:43 +0800

    [Info] Add routes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'pages/pray.dart';
+import 'pages/donate.dart';
+import 'pages/latestNews.dart';
+import 'pages/home.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,53 +18,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+
+      routes: {
+        '/home': (BuildContext context) =>
+            HomeScaffold(),
+        '/latest': (BuildContext context) =>
+            LatestNewsScaffold(title : "最新文章"),
+        '/pray' : (BuildContext context) =>
+            PrayScaffold(),
+        '/donate' : (BuildContext context) =>
+            DonateScaffold(),
+      },
+      initialRoute: '/home',
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
