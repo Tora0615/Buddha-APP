7ca8408166f276dbb2a71bb40b654d2bf8855e62
Tora0615 <5438arthur@gmail.com>
2021-07-25 18:26:53 +0800

    feat : Create CommonHeader Widget.
    1. Have logo row in the top.
    2. Have info row in the second line.
    3. Have picture widget in the third line.
    4. Have page change button at the fourth line. (now it is just text)

--- lib/pages/latestNews.dart
+++ lib/pages/latestNews.dart
@@ -1,23 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'commonWidget/CommonHeader.dart';
 
 class LatestNewsScaffold extends StatelessWidget {
-
   final title;
   LatestNewsScaffold({Key? key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          "功能開發中，敬請期待",
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
+      //appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        child: CommonHeader(headerPicPath: "images/pray/bg_pray.jpg",),
       ),
     );
   }
