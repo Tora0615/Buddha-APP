6be5c64b51197a0c71adc7ed29bfcccdb5a838d8
Tora0615 <5438arthur@gmail.com>
2021-07-26 01:22:12 +0800

    feat : Complete WidgetBeforePray and some comments.

--- lib/pages/pray.dart
+++ lib/pages/pray.dart
@@ -75,7 +75,6 @@ class PrayScaffold extends StatelessWidget {
   }
 }
 
-
 // 除了共用 header 外的內容
 class PrayContext extends StatefulWidget {
   const PrayContext({Key? key}) : super(key: key);
@@ -98,14 +97,17 @@ class _PrayContextState extends State<PrayContext> {
   Widget build(BuildContext context) {
     //切換 widget 時，若大小有改變會順便有動畫過度
     return AnimatedCrossFade(
-        duration: const Duration(milliseconds: 500 ),
-        firstChild: WidgetAfterPray(),
-        secondChild: WidgetBeforePray(changeBoolState: changeBoolState),
-        crossFadeState: isAfterPray ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+      duration: const Duration(milliseconds: 500),
+      firstChild: WidgetAfterPray(),
+      secondChild: WidgetBeforePray(changeBoolState: changeBoolState),
+      crossFadeState:
+          isAfterPray ? CrossFadeState.showFirst : CrossFadeState.showSecond,
     );
   }
 }
 
+
+// 按下占卜按鈕前頁面，內容為介紹等等
 class WidgetBeforePray extends StatelessWidget {
   final VoidCallback changeBoolState; //
   const WidgetBeforePray({Key? key, required this.changeBoolState})
@@ -113,22 +115,70 @@ class WidgetBeforePray extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('地藏占卜'),
-        Text(
-            '''        地藏占卜程式是以「地藏十輪經」為基礎，所開發出來的權巧應用。云云眾生身處無邊煩惱之五濁穢世，心神難免經常有所徬徨，對於茫茫前程不知如何進退。此一程式藉由地藏菩薩無限威神力之助益，可以拈取迷者本身之身、口、意三業力之作用總合，給予即將應驗之果報的預警。欲使用此程式占卜未來果報之迷者，應誠心默唸「地藏王菩薩」之名號十遍，祈求菩薩慈悲開示未來果報吉凶，並助迷者善因善緣相親，惡因惡緣速離 !!!'''),
-        Text('誠心默念'),
-        for (var i = 0; i < 10; i++) Text('南無地藏王菩薩'),
-        ElevatedButton(
-          onPressed: changeBoolState,
-          child: Text('按此抽籤'),
-        )
-      ],
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text(
+            '地藏占卜',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            '        地藏占卜程式是以「地藏十輪經」為基礎，所開發出來的權巧應用。云云眾生身處無邊煩惱之五濁穢世，心神難免經常有所徬徨，對於茫茫前程不知如何進退。',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '        此一程式藉由地藏菩薩無限威神力之助益，可以拈取迷者本身之身、口、意三業力之作用總合，給予即將應驗之果報的預警。',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '        欲使用此程式占卜未來果報之迷者，應誠心默唸「地藏王菩薩」之名號十遍，祈求菩薩慈悲開示未來果報吉凶，並助迷者善因善緣相親，惡因惡緣速離 !!!',
+            style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '誠心默念',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          for (var i = 0; i < 10; i++)
+            Column(
+              children: [
+                Text(
+                  '南無地藏王菩薩',
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10,),
+              ],
+            ),
+          ElevatedButton(
+            onPressed: changeBoolState,
+            child: Text('按此抽籤',style: TextStyle(color: Colors.black),),
+            style: ElevatedButton.styleFrom(
+              elevation: 2,
+              primary: Colors.amber,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
+// 按下占卜按鈕後頁面，內容為折疊起來的籤詩
 class WidgetAfterPray extends StatelessWidget {
   final Random random = Random();
   WidgetAfterPray({Key? key}) : super(key: key);
@@ -673,7 +723,7 @@ class FoldingPoem extends StatelessWidget {
               style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
             ),
             Text(
-              "您抽到第" + (poemIndex + 1).toString() + "號籤文",
+              "您抽到的是第" + (poemIndex + 1).toString() + "號籤文",
               style: TextStyle(
                 fontSize: 28,
               ),
